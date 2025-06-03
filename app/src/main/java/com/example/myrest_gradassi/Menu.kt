832f3bbd8e29329db5a5d8290d6a0d47d3cf5483
TommasoGradassi <tommasogradassi811@gmail.com>
2025-06-03 12:32:30 +0200

    Modifiche relative al Menu

--- app/src/main/java/com/example/myrest_gradassi/Menu.kt
+++ app/src/main/java/com/example/myrest_gradassi/Menu.kt
@@ -1,14 +1,40 @@
-package com.example.myrest_gradassi
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class Menu : AppCompatActivity() {
+class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_menu)
+
+        var btnSearch : Button = findViewById(R.id.btnFind)
+        var btnWebView : Button = findViewById(R.id.btnBrowse)
+        var btnRest : Button = findViewById(R.id.btnRest)
+
+        btnSearch.setOnClickListener(View.OnClickListener {
+            var toast = Toast.makeText(this, "Search", Toast.LENGTH_LONG)
+            toast.show()
+            val intent = Intent(this, SearchActivity::class.java)
+            startActivity(intent)
+        })
+
+        btnWebView.setOnClickListener(View.OnClickListener {
+            var toast = Toast.makeText(this, "WebView", Toast.LENGTH_LONG)
+            toast.show()
+            val intent = Intent(this, WebViewActivity::class.java)
+            startActivity(intent)
+        })
+
+        btnRest.setOnClickListener(View.OnClickListener {
+            var toast = Toast.makeText(this, "Rest", Toast.LENGTH_LONG)
+            toast.show()
+            val intent = Intent(this, RestActivity::class.java)
+            startActivity(intent)
+        })
     }
 }
