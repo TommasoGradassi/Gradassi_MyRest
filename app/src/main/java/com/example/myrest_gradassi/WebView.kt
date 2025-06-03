62990a091149bf5e8720f47fdcf7fbd371f42257
TommasoGradassi <tommasogradassi811@gmail.com>
2025-06-03 12:35:31 +0200

    Correzione errori

--- app/src/main/java/com/example/myrest_gradassi/WebView.kt
+++ app/src/main/java/com/example/myrest_gradassi/WebView.kt
@@ -1,14 +1,27 @@
-package com.example.myrest_gradassi
 import android.os.Bundle
+import android.webkit.WebView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class WebView : AppCompatActivity() {
+class WebViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_web_view)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val myWebView = WebView(applicationContext)
+        myWebView.settings.javaScriptEnabled = true
+        setContentView(myWebView)
+        myWebView.loadUrl("https://www.justwatch.com/it/provider/now-tv/film")
+
+
     }
 }
